7e999b8e2108068f20f113b4cc0071d75f89992c
blandco <jbland@gmail.com>
2022-09-07 12:25:38 -0500

    fixes code formatting

--- app/src/main/java/com/joelbland/trafficlight/MainActivity.java
+++ app/src/main/java/com/joelbland/trafficlight/MainActivity.java
@@ -32,29 +32,29 @@ public class MainActivity extends AppCompatActivity {
 
         // OnClick listener for the button.
         button.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View view) {
+              @Override
+              public void onClick(View view) {
 
-                                          // Get the text of the button to use in our control structure.
-                                          String buttonText = button.getText().toString();
+                  // Get the text of the button to use in our control structure.
+                  String buttonText = button.getText().toString();
 
-                                          // Compare the button text and set the next light.
-                                          if (buttonText.equals("STOP")) {
-                                              button.setText("GO");
-                                              button.setBackgroundColor(GREEN);
-                                              lights.setImageDrawable(green);
-                                          } else if (buttonText.equals("GO")) {
-                                              button.setText("WAIT");
-                                              button.setBackgroundColor(YELLOW);
-                                              lights.setImageDrawable(yellow);
-                                          } else {
-                                              button.setText("STOP");
-                                              button.setBackgroundColor(RED);
-                                              lights.setImageDrawable(red);
-                                          }
+                  // Compare the button text and set the next light.
+                  if (buttonText.equals("STOP")) {
+                      button.setText("GO");
+                      button.setBackgroundColor(GREEN);
+                      lights.setImageDrawable(green);
+                  } else if (buttonText.equals("GO")) {
+                      button.setText("WAIT");
+                      button.setBackgroundColor(YELLOW);
+                      lights.setImageDrawable(yellow);
+                  } else {
+                      button.setText("STOP");
+                      button.setBackgroundColor(RED);
+                      lights.setImageDrawable(red);
+                  }
 
-                                      }
-                                  });
+              }
+          });
     }
 }
 
