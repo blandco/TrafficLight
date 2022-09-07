0ee6e73e19caf7207f240624dc5b5215083ccf9a
blandco <jbland@gmail.com>
2022-09-07 12:09:15 -0500

    changes button to use a setOnClickListener method in the MainActivity controller

--- app/src/main/java/com/joelbland/trafficlight/MainActivity.java
+++ app/src/main/java/com/joelbland/trafficlight/MainActivity.java
@@ -20,8 +20,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Get the IDs for the View resources
+        Button button = findViewById(R.id.button);
+        ImageView lights = findViewById(R.id.imageView);
+
+        // Get the Drawable resources for the three different lights
+        Drawable red = getDrawable(R.drawable.traffic_lights_red);
+        Drawable green = getDrawable(R.drawable.traffic_lights_green);
+        Drawable yellow = getDrawable(R.drawable.traffic_lights_yellow);
+
+        // OnClick listener for the button.
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View view) {
+
+                                          // Get the text of the button to use in our control structure.
+                                          String buttonText = button.getText().toString();
+
+                                          // Compare the button text and set the next light.
+                                          if (buttonText.equals("STOP")) {
+                                              button.setText("GO");
+                                              button.setBackgroundColor(GREEN);
+                                              lights.setImageDrawable(green);
+                                          } else if (buttonText.equals("GO")) {
+                                              button.setText("WAIT");
+                                              button.setBackgroundColor(YELLOW);
+                                              lights.setImageDrawable(yellow);
+                                          } else {
+                                              button.setText("STOP");
+                                              button.setBackgroundColor(RED);
+                                              lights.setImageDrawable(red);
+                                          }
+
+                                      }
+                                  });
+    }
+}
+
+ /**
     public void change_light(View view) {
 
         // Get the IDs for the View resources
@@ -54,4 +91,4 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-}
+}*/
