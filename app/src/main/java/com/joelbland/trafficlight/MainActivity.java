0eea05d4e89c787a04ce0d8748d48159a90c5b41
blandco <jbland@gmail.com>
2022-09-07 10:16:50 -0500

    adds control structure

--- app/src/main/java/com/joelbland/trafficlight/MainActivity.java
+++ app/src/main/java/com/joelbland/trafficlight/MainActivity.java
@@ -1,8 +1,18 @@
 package com.joelbland.trafficlight;
 
+import static android.graphics.Color.GREEN;
+import static android.graphics.Color.RED;
+import static android.graphics.Color.YELLOW;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableWrapper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void change_light(View view) {
+
+        // Get the IDs for the View resources
+        Button button = findViewById(R.id.button);
+        ImageView lights = findViewById(R.id.imageView);
+
+        // Get the Drawable resources for the three different lights
+        Drawable red = getDrawable(R.drawable.traffic_lights_red);
+        Drawable green = getDrawable(R.drawable.traffic_lights_green);
+        Drawable yellow = getDrawable(R.drawable.traffic_lights_yellow);
+
+        // Get the text of the button to use in our control structure.
+        String buttonText = button.getText().toString();
+
+        // Compare the button text and set the next light.
+        if (buttonText.equals("STOP")){
+            button.setText("GO");
+            button.setBackgroundColor(GREEN);
+            lights.setImageDrawable(green);
+        }
+        else if (buttonText.equals("GO")){
+            button.setText("WAIT");
+            button.setBackgroundColor(YELLOW);
+            lights.setImageDrawable(yellow);
+        }
+        else {
+            button.setText("STOP");
+            button.setBackgroundColor(RED);
+            lights.setImageDrawable(red);
+        }
+
     }
 }
