30c6eb92ba2fc3491d6e4577567e8ffc50beefc8
blandco <jbland@gmail.com>
2022-09-11 12:26:58 -0500

    adds comments and removes old code

--- app/src/main/java/com/joelbland/trafficlight/MainActivity.java
+++ app/src/main/java/com/joelbland/trafficlight/MainActivity.java
@@ -3,19 +3,14 @@ package com.joelbland.trafficlight;
 import static android.graphics.Color.GREEN;
 import static android.graphics.Color.RED;
 import static android.graphics.Color.YELLOW;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.DrawableWrapper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,15 +19,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get the IDs for the View resources
+        // Get the ID for each View object.
+
+        // A Button object the user clicks to change the traffic light.
         Button button = findViewById(R.id.button);
+
+        // An image object for the light colors.
         ImageView lights = findViewById(R.id.imageView);
+
+        // An ImageButton object the user clicks to change the background.
         ImageButton backgroundButton = findViewById(R.id.imageButton);
 
-        // ConstraintLayout object needs to have @+id assigned in activity_main.xml
+        // !!Remember!! ConstraintLayout object needs to have @+id assigned in activity_main.xml
         ConstraintLayout layout = findViewById(R.id.constraintLayout);
 
-        // Get the Drawable resources for the three different lights
+        // Get the Drawable resources for the three different lights.
         Drawable red = getDrawable(R.drawable.traffic_lights_red);
         Drawable green = getDrawable(R.drawable.traffic_lights_green);
         Drawable yellow = getDrawable(R.drawable.traffic_lights_yellow);
@@ -46,30 +47,24 @@ public class MainActivity extends AppCompatActivity {
         // so we can change it later in the backgroundButton.setOnClickListener
         layout.setBackground(city);
 
-        // Go ahead and set the button text so our control structure works
-        // on the first click!!!! Now I can sleep.
-        // button.setText("STOP");
-
-        // Create the TrafficLight object and set the attributes.
+        // Create a new TrafficLight object and set the initial attributes.
         TrafficLight trafficLight = new TrafficLight();
         trafficLight.setLightColor("RED");
         trafficLight.setLightText("STOP");
-
 
         // OnClick listener for the button.
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                // Debug output
+                // DEBUG OUTPUT //
                 System.out.println("CURRENT COLOR: " + trafficLight.getLightColor());
                 System.out.println("NEXT COLOR: " + trafficLight.getNextLightColor());
                 System.out.println("CURRENT TEXT: " + trafficLight.getLightText());
                 System.out.println("NEXT TEXT: " + trafficLight.getNextLightText());
-                // End Debug output
+                // END DEBUG OUTPUT //
 
-
-                // Control structure to determine which background and button color to use.
+                // Control structure to set the next button color and light image.
                 if (trafficLight.getLightColor().equals("RED")){
                     button.setBackgroundColor(GREEN);
                     lights.setImageDrawable(green);
@@ -83,33 +78,12 @@ public class MainActivity extends AppCompatActivity {
                     lights.setImageDrawable(red);
                 }
 
-                // Call the getNext methods and set the next light and text colors.
+                // Call both of the getNextLight methods and set the next light and text colors.
                 trafficLight.setLightColor(trafficLight.getNextLightColor());
                 trafficLight.setLightText(trafficLight.getNextLightText());
 
                 // Set the button text to the next light text.
                 button.setText(trafficLight.getLightText());
-/*
-
-                  // Get the text of the button to use in our control structure.
-                  // String buttonText = button.getText().toString();
-
-                  // Compare the button text and set the next light.
-                  if (buttonText.equals("STOP")) {
-                      button.setText("GO");
-                      button.setBackgroundColor(GREEN);
-                      lights.setImageDrawable(green);
-                  } else if (buttonText.equals("GO")) {
-                      button.setText("WAIT");
-                      button.setBackgroundColor(YELLOW);
-                      lights.setImageDrawable(yellow);
-                  } else {
-                      button.setText("STOP");
-                      button.setBackgroundColor(RED);
-                      lights.setImageDrawable(red);
-                  }
-*/
-
             }
         });
 
@@ -118,12 +92,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                // Debug output to compare the names of drawables.
+                // DEBUG OUTPUT //
+                // TO COMPARE THE NAMES OF DRAWABLES. //
                 // System.out.println(layout.getBackground());
                 // System.out.println(city);
                 // System.out.println(city2);
                 // System.out.println(city3);
+                // END DEBUG OUTPUT //
 
+                // Control structure to set the next background image.
                 if (layout.getBackground() == city) {
                     layout.setBackground(city2);
                 }
@@ -133,46 +110,7 @@ public class MainActivity extends AppCompatActivity {
                 else {
                     layout.setBackground(city);
                 }
-
-
-
-
             }
         });
     }
 }
-
-/**
- public void change_light(View view) {
-
- // Get the IDs for the View resources
- Button button = findViewById(R.id.button);
- ImageView lights = findViewById(R.id.imageView);
-
- // Get the Drawable resources for the three different lights
- Drawable red = getDrawable(R.drawable.traffic_lights_red);
- Drawable green = getDrawable(R.drawable.traffic_lights_green);
- Drawable yellow = getDrawable(R.drawable.traffic_lights_yellow);
-
- // Get the text of the button to use in our control structure.
- String buttonText = button.getText().toString();
-
- // Compare the button text and set the next light.
- if (buttonText.equals("STOP")){
- button.setText("GO");
- button.setBackgroundColor(GREEN);
- lights.setImageDrawable(green);
- }
- else if (buttonText.equals("GO")){
- button.setText("WAIT");
- button.setBackgroundColor(YELLOW);
- lights.setImageDrawable(yellow);
- }
- else {
- button.setText("STOP");
- button.setBackgroundColor(RED);
- lights.setImageDrawable(red);
- }
-
- }
- }*/
